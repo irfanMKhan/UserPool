d285575ae3daa608801d5d6fc19bd852f6c3423a
irfanMKhan <irfanupdate@yahoo.com>
2025-04-22 11:52:41 -0500

    initial commit

---
+++ src/main/java/com/secured/userpool/UserPoolApplication.java
@@ -0,0 +1,13 @@
+package com.secured.userpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserPoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/secured/userpool/UserPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secured.userpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
